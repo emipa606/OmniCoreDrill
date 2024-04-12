b73e16eec94f6e3d8de16714afeb916c5a923df4
Mlie <emipa606@users.noreply.github.com>
2024-04-12 16:53:51 +0200

    Should now be able to mine resources only defined as available for the deep-miner

--- Source/OmniCoreDrill/ThingDefGenerator.cs
+++ Source/OmniCoreDrill/ThingDefGenerator.cs
@@ -26,19 +26,20 @@ public static class ThingDefGenerator
         _miningRecipes = new Dictionary<ThingDef, RecipeDef>();
         _drillProperties = new Dictionary<ThingDef, DrillingProperties>();
         _sourceLabels = new Dictionary<ThingDef, string>();
+        var minableDefs = DefDatabase<ThingDef>.AllDefs.Where(td =>
+            td.mineable && td.building?.mineableThing != null);
 
-        foreach (var source in DefDatabase<ThingDef>.AllDefs.Where(td =>
-                     td.mineable && td.building?.mineableThing != null))
+        foreach (var thingDef in minableDefs)
         {
-            var props = CalculateDrillProperties(source, out var b);
+            var props = CalculateDrillProperties(thingDef, out var buildingProperties);
             if (props == null)
             {
                 Log.Warning(
-                    $"Cannot determine hitpoints for {source.defName} - skipping deep resource extraction recipe.");
+                    $"Cannot determine hitpoints for {thingDef.defName} - skipping deep resource extraction recipe.");
                 continue;
             }
 
-            var t = b.mineableThing;
+            var mineableThing = buildingProperties.mineableThing;
 
             var recipe = new RecipeDef
             {
@@ -47,14 +48,14 @@ public static class ThingDefGenerator
                 effectWorking = EffecterDefOf.Drill,
                 workSkillLearnFactor = 0.2f,
                 workSkill = SkillDefOf.Mining,
-                defName = $"OCD_MineDeep{t.defName}",
-                label = LanguageKeys.keyed.ocd_label.Translate(t.LabelCap),
-                jobString = LanguageKeys.keyed.ocd_jobString.Translate(t.LabelCap),
+                defName = $"OCD_MineDeep{mineableThing.defName}",
+                label = LanguageKeys.keyed.ocd_label.Translate(mineableThing.LabelCap),
+                jobString = LanguageKeys.keyed.ocd_jobString.Translate(mineableThing.LabelCap),
                 products =
                 [
                     new ThingDefCountClass
                     {
-                        thingDef = t,
+                        thingDef = mineableThing,
                         count = 0
                     }
                 ],
@@ -62,23 +63,74 @@ public static class ThingDefGenerator
                 unfinishedThingDef = DefReferences.Thing_UnfinishedDrillingPlan
             };
 
-            UpdateGeneratedDef(t, recipe, props, source.LabelCap);
+            UpdateGeneratedDef(mineableThing, recipe, props, thingDef.LabelCap);
 
-            _miningRecipes[t] = recipe;
-            _drillProperties[t] = props;
-            _sourceLabels[t] = source.LabelCap;
+            _miningRecipes[mineableThing] = recipe;
+            _drillProperties[mineableThing] = props;
+            _sourceLabels[mineableThing] = thingDef.LabelCap;
+        }
+
+        foreach (var mineableThing in DefDatabase<ThingDef>.AllDefs.Where(td => td.deepCommonality > 0))
+        {
+            if (_miningRecipes.ContainsKey(mineableThing))
+            {
+                continue;
+            }
+
+            var nearestThingDef = minableDefs
+                .OrderBy(def => Math.Abs(def.building.mineableThing.BaseMarketValue - mineableThing.BaseMarketValue))
+                .First();
+            Log.Message(
+                $"Using {nearestThingDef} as a substitute for {mineableThing} to calculate deep mining values.");
+
+            var props = CalculateDrillProperties(nearestThingDef, out _);
+            if (props == null)
+            {
+                Log.Warning(
+                    $"Cannot determine hitpoints for {mineableThing.defName} - skipping deep resource extraction recipe.");
+                continue;
+            }
+
+            var recipe = new RecipeDef
+            {
+                efficiencyStat = StatDefOf.MiningYield,
+                workSpeedStat = StatDefOf.DeepDrillingSpeed,
+                effectWorking = EffecterDefOf.Drill,
+                workSkillLearnFactor = 0.2f,
+                workSkill = SkillDefOf.Mining,
+                defName = $"OCD_MineDeep{mineableThing.defName}",
+                label = LanguageKeys.keyed.ocd_label.Translate(mineableThing.LabelCap),
+                jobString = LanguageKeys.keyed.ocd_jobString.Translate(mineableThing.LabelCap),
+                products =
+                [
+                    new ThingDefCountClass
+                    {
+                        thingDef = mineableThing,
+                        count = 0
+                    }
+                ],
+                recipeUsers = [DefReferences.Thing_CoreDrill],
+                unfinishedThingDef = DefReferences.Thing_UnfinishedDrillingPlan
+            };
+
+            UpdateGeneratedDef(mineableThing, recipe, props, mineableThing.LabelCap);
+
+            _miningRecipes[mineableThing] = recipe;
+            _drillProperties[mineableThing] = props;
+            _sourceLabels[mineableThing] = mineableThing.LabelCap;
         }
 
         return _miningRecipes.Values;
     }
 
-    private static DrillingProperties CalculateDrillProperties(ThingDef source, out BuildingProperties b)
+    private static DrillingProperties CalculateDrillProperties(ThingDef source,
+        out BuildingProperties buildingProperties)
     {
-        b = source.building;
+        buildingProperties = source.building;
 
         var hitpointsPerLump = source.statBases.FirstOrDefault(sm => sm.stat == StatDefOf.MaxHitPoints)?.value;
 
-        return hitpointsPerLump == null ? null : new DrillingProperties(hitpointsPerLump.Value, b);
+        return hitpointsPerLump == null ? null : new DrillingProperties(hitpointsPerLump.Value, buildingProperties);
     }
 
     public static void UpdateGeneratedDef(ThingDef material)
